9b7d80f1bd5b7818012e4d3f0af05188e2feda09
simon <simon200093@163.com>
2020-11-27 00:07:01 +0800

    changetest

--- XUnitTestProject1/accountServiceTest.cs
+++ XUnitTestProject1/accountServiceTest.cs
@@ -2,14 +2,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using accountService;
-using accountService.Controllers;
-using accountService.Data;
 using accountService.Models;
-using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -49,38 +44,12 @@ namespace XUnitTestProject1
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
-
-        [Fact]
-        public async Task PutAccount_Ok()
-        {
-            var id = 2;
-                    
-        var content = new StringContent(JsonSerializer.Serialize(new Account{Id=3, UserName = "faker", UserEmail = "gg@123.com", Password = "123", UserBio = "hiagsfbkjas", UserType = "landlord", UserMobile = 134542 }), Encoding.UTF8, "application/json");
-
-            // Act
-            var response = await Client.PutAsync($"/Accounts/{id}", content);
-
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        }
-
         [Fact]
         public async Task PostAccount_Ok()
         {
             var content = new StringContent(JsonSerializer.Serialize(new Account { UserName = "test", UserEmail = "gg@123.com", Password = "123", UserBio = "hiagsfbkjas", UserType = "landlord", UserMobile = 134542 }), Encoding.UTF8, "application/json");
             // Act
             var response = await Client.PostAsync($"/Accounts/register", content);
-
-            // Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-        }
-
-        [Fact]
-        public async Task DeleteAccount_Ok()
-        {
-            var id = 2;
-            // Act
-            var response = await Client.DeleteAsync($"/Accounts/{id}");
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
